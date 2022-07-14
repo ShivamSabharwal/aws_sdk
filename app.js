76a87ad745c9d2cd6a35c041b02509e5cd3a8bc0
Shivam Sabharwal <shivamwatson55@gmail.com>
2022-07-14 12:37:09 +0530

    connection using node made

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 // const AWS = require('aws-sdk');
+const express = require("express");
+const app = express();
 
 const {
   EventBridgeClient,
   ActivateEventSourceCommand,
 } = require("@aws-sdk/client-eventbridge");
 
-const client = new EventBridgeClient({ region: "REGION" });
+const client = new EventBridgeClient({ region: "ap-south-1" });
 
-const params = {
-  /** input parameters */
-};
-const command = new ActivateEventSourceCommand(params);
-
-// callbacks.
-client.send(command, (err, data) => {
-  // process err and data.
+app.get("/", function (res, req) {
+  console.log("HI");
+  const params = {
+    /** input parameters */
+  };
+  const command = new ActivateEventSourceCommand(params);
+  console.log(client);
+  // callbacks.
+  client.send(command, (err, data) => {
+    // process err and data.
+    if (err) {
+      res.send(err);
+      throw err;
+    } else {
+      res.send(data);
+    }
+  });
+});
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
 });
